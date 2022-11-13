3d89b014d055b1ace8469683b2ada295070a69cf
pgorecky <patryk.gorecky@gmail.com>
2022-11-13 20:47:38 +0100

    Zadanie 3-4

--- src/com/pgorecky/Human.java
+++ src/com/pgorecky/Human.java
@@ -1,5 +1,8 @@
 package com.pgorecky;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Human {
     String firstName;
     String lastName;
@@ -9,11 +12,31 @@ public class Human {
     Integer yearOfBirth;
     private Phone phone;
     public Car car;
-
+    LocalDateTime ldt;
+    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
     public Human(String firstName, String lastName, Integer yearOfBirth){
         this.firstName = firstName;
         this.lastName = lastName;
         this.yearOfBirth = yearOfBirth;
+    }
+    public void setSalary(Double salary){
+        if (salary < 0){
+            System.out.println("The reported payout value is negative!");
+            System.out.println("Changes cannot be made!");
+        } else {
+            ldt = LocalDateTime.now();
+            System.out.println("The change has been successful!");
+            System.out.println("*new data has been sent to the accounting system.");
+            System.out.println("Annex to the contract is waiting in the office of Mrs. Hanna from Human resources.");
+            System.out.println("The ZUS and the US have already been informed of your raise, so you'd better not hide it!");
+            this.previousSalary = this.salary;
+            this.salary = salary;
+        }
+    }
+    public void getInformationAboutSalary(){
+        System.out.println("The value of the payout last time was changed a day: " + dtf.format(ldt));
+        System.out.println("The salary before the changes was: $" + previousSalary);
+        System.out.println("The current salary is: $" + salary);
     }
 
 }
--- src/com/pgorecky/Main.java
+++ src/com/pgorecky/Main.java
@@ -6,7 +6,11 @@ public class Main {
         Animal dog = new Animal("dog");
         dog.name = "Snowy";
         me.pet = dog;
+        dog.takeForAWalk();
+        dog.feed();
         Car auto = new Car("Vanquish S", "Aston Martin", 520, 253000);
         me.car = auto;
+        me.setSalary(16000.0);
+        me.getInformationAboutSalary();
     }
 }
